7d3d39048c44a43c223a45eecdde60aa8c102196
JoseK03 <dajoseva@gmail.com>
2023-09-28 15:55:13 -0500

    feat: :tada: Creacion video 6

---
+++ video6/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const url = 'https://randomuser.me/api/';
+  const peticion = fetch(url);
+  
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default App;
