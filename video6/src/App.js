dc84dc21e37048937cdddbf5c47732ee278ce68d
JoseK03 <dajoseva@gmail.com>
2023-09-28 16:33:18 -0500

    feat: :construction: Mitad de ejercicio

--- video6/src/App.js
+++ video6/src/App.js
@@ -1,13 +1,33 @@
-import logo from './logo.svg';
+import {useEffect , useState} from 'react'
 import './App.css';
 
 function App() {
-  const url = 'https://randomuser.me/api/';
-  const peticion = fetch(url);
+  const [datos, setDatos] = useState([]);
+  useEffect(()=>{
+    const url = 'https://randomuser.me/api/?results=3';
+    const peticion = fetch(url);
+    peticion
+    .then(datos=> datos.json())
+    .then(lectura =>{
+      lectura.result.map((persona)=>{
+        setDatos(
+        <div key={persona.email}>
+          <div>{persona.name.first} {persona.name.last}</div>
+          <div>
+            <img src={persona.picture.large}/>
+          </div>
+        </div>)
+        console.log(persona.name.first);
+        
+        })
+      })
+    .catch(()=>console.log('mal'))
+  },[])
   
   return (
     <>
-    
+    <h1>Empleado/a del mes</h1>
+    {datos}
     </>
   );
 }
